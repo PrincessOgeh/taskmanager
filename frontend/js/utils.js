714504094bf44873c129efb1281282095ae3322d
Princess <princessogechi94@gmail.com>
2024-11-27 15:34:52 +0100

    second commit

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://taskmanager-backend-zsph.onrender.com'; // Base URL for the API
+const apiUrl = 'https://taskmanager-sp8u.onrender.com'; // Base URL for the API
 
 // Generic GET request
 async function getData(endpoint, token = '') {
